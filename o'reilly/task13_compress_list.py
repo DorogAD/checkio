91ff4341df2c0f147bdcb631fb0f087bf64629b4
DorogAD <sa_do@tut.by>
2020-12-23 18:14:52 +0300

    solved task13 o'reilly

---
+++ o'reilly/task13_compress_list.py
@@ -0,0 +1,43 @@
+"""
+A given list should be "compressed" in a way so, instead of two (or more) equal elements, staying one after another,
+there is only one in the result Iterable (list, tuple, iterator ...)
+
+Input: List.
+
+Output: "Compressed" Iterable (list, tuple, iterator ...).
+"""
+from typing import Iterable
+
+
+def compress(items: list) -> Iterable:
+    result = []
+    for i in range(len(items)):
+        if i == 0:
+            result.append(items[i])
+        elif items[i] != items[i - 1]:
+            result.append(items[i])
+    return result
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(list(compress([
+        5, 5, 5,
+        4, 5, 6,
+        6, 5, 5,
+        7, 8, 0,
+        0])))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert list(compress([
+        5, 5, 5,
+        4, 5, 6,
+        6, 5, 5,
+        7, 8, 0,
+        0])) == [5, 4, 5, 6, 5, 7, 8, 0]
+    assert list(compress([1, 1, 1, 1, 2, 2, 2, 1, 1, 1])) == [1, 2, 1]
+    assert list(compress([7, 7])) == [7]
+    assert list(compress([])) == []
+    assert list(compress([1, 2, 3, 4])) == [1, 2, 3, 4]
+    assert list(compress([9, 9, 9, 9, 9, 9, 9])) == [9]
+    print("Coding complete? Click 'Check' to earn cool rewards!")
