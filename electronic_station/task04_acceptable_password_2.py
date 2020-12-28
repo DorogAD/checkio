14c75b215bb7cd4d2cfcd75084546a6df4b24a8d
DorogAD <sa_do@tut.by>
2020-12-28 15:05:15 +0300

    solved task04 electronic

---
+++ electronic_station/task04_acceptable_password_2.py
@@ -0,0 +1,44 @@
+"""
+In this mission you need to create a password verification function.
+
+Those are the verification conditions:
+
+    the length should be bigger than 6;
+    should contain at least one digit.
+
+Input: A string.
+
+Output: A bool.
+"""
+
+
+def is_acceptable_password(password: str) -> bool:
+    return True if len(password) > 6 else False
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(is_acceptable_password('short'))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert is_acceptable_password('short') == False
+    assert is_acceptable_password('muchlonger') == True
+    assert is_acceptable_password('ashort') == False
+    print("Coding complete? Click 'Check' to earn cool rewards!")
+
+
+def is_acceptable_password(password: str) -> bool:
+    return len(password) > 6 and any([x.isdigit() for x in password])
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(is_acceptable_password('short'))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert is_acceptable_password('short') == False
+    assert is_acceptable_password('muchlonger') == False
+    assert is_acceptable_password('ashort') == False
+    assert is_acceptable_password('muchlonger5') == True
+    assert is_acceptable_password('sh5') == False
+    print("Coding complete? Click 'Check' to earn cool rewards!")
