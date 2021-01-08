f1f000dabf7a0d3e54865c8ca8e438ae7777779e
DESKTOP-AlexDorog <ad.dorog@gmail.com>
2021-01-08 13:07:36 +0300

    scientific_expedition task09 v3

--- scientific_expedition/task09_follow_instructions_v3.py
+++ scientific_expedition/task09_follow_instructions_v3.py
@@ -33,7 +33,6 @@ if __name__ == '__main__':
 
     # These "asserts" are used for self-checking and not for an auto-testing
     assert follow("fflff") == (-1, 4)
-    assert follow("fflff") == (-1, 4)
     assert follow("ffrff") == (1, 4)
     assert follow("fblr") == (0, 0)
     print("Coding complete? Click 'Check' to earn cool rewards!")
