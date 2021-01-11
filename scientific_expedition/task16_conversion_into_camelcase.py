ea51ea0f93df6bffe58862745b125fae73512219
DorogAD <sa_do@tut.by>
2021-01-11 15:03:20 +0300

    scientific_expedition task16 finish

--- scientific_expedition/task16_conversion_into_camelcase.py
+++ scientific_expedition/task16_conversion_into_camelcase.py
@@ -9,15 +9,18 @@ Output: The same string, but in CamelCase.
 """
 
 
-def to_camel_case(name):
-    #replace this for solution
-    return name
+def to_camel_case(name: str) -> str:
+    result = ''
+    for word in name.split('_'):
+        result += word.title()
+    return result
+
 
 if __name__ == '__main__':
     print("Example:")
     print(to_camel_case('name'))
 
-    #These "asserts" using only for self-checking and not necessary for auto-testing
+    # These "asserts" using only for self-checking and not necessary for auto-testing
     assert to_camel_case("my_function_name") == "MyFunctionName"
     assert to_camel_case("i_phone") == "IPhone"
     assert to_camel_case("this_function_is_empty") == "ThisFunctionIsEmpty"
