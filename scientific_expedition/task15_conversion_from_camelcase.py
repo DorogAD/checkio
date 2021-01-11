08bb7b173e6daf286729abac330ef762e73b430a
DorogAD <sa_do@tut.by>
2021-01-11 13:33:34 +0300

    scientific_expedition task15 finish

--- scientific_expedition/task15_conversion_from_camelcase.py
+++ scientific_expedition/task15_conversion_from_camelcase.py
@@ -9,15 +9,21 @@ Output: The same string, but in under_score.
 """
 
 
-def from_camel_case(name):
-    #replace this for solution
-    return name
+def from_camel_case(name: str) -> str:
+    result = ''
+    for letter in name:
+        if letter.isupper() and letter != name[0]:
+            result += '_' + letter.lower()
+        else:
+            result += letter.lower()
+    return result
+
 
 if __name__ == '__main__':
     print("Example:")
     print(from_camel_case("Name"))
 
-    #These "asserts" using only for self-checking and not necessary for auto-testing
+    # These "asserts" using only for self-checking and not necessary for auto-testing
     assert from_camel_case("MyFunctionName") == "my_function_name"
     assert from_camel_case("IPhone") == "i_phone"
     assert from_camel_case("ThisFunctionIsEmpty") == "this_function_is_empty"
