0eb8c42702cb5fb4dbf633c9db21e4e6d889d328
DorogAD <sa_do@tut.by>
2021-01-15 17:19:30 +0300

    scientific_expedition task20 finish

--- scientific_expedition/task20_yaml_more_types.py
+++ scientific_expedition/task20_yaml_more_types.py
@@ -43,6 +43,7 @@ Input: A format string.
 Output: An object.
 """
 # Taken from mission YAML. Simple Dict
+import re
 
 
 def yaml(a: str) -> dict:
@@ -69,51 +70,68 @@ class: 12b""") == {'age': 12,
     print("Coding complete? Click 'Check' to earn cool rewards!")
 
 
-def yaml(a):
-    # your code here
-    return None
+def yaml_2(a: str) -> dict:
+    string_list = [re.sub(":$", ": null", pair) for pair in a.splitlines() if pair]
+    list_key = []
+    list_value = []
+    for pair in string_list:
+        list_key.append(pair.split(': ')[0])
+        list_value.append(pair.split(': ')[1])
+    new_values = []
+    for value in list_value:
+        if value == 'false':
+            new_values.append(False)
+        if value == 'null':
+            new_values.append(None)
+        if value.isdigit():
+            new_values.append(int(value))
+        else:
+            value = value.replace('"', '').replace(chr(92), '"')
+            new_values.append(value.strip())
+    result = dict(zip(list_key, new_values))
+    return result
 
 
 if __name__ == '__main__':
     print("Example:")
-    print(yaml('name: Alex\nage: 12'))
+    print(yaml_2('name: Alex\nage: 12'))
 
     # These "asserts" are used for self-checking and not for an auto-testing
-    assert yaml('name: Alex\nage: 12') == {'age': 12, 'name': 'Alex'}
-    assert yaml('name: Alex Fox\n'
+    assert yaml_2('name: Alex\nage: 12') == {'age': 12, 'name': 'Alex'}
+    assert yaml_2('name: Alex Fox\n'
      'age: 12\n'
      '\n'
      'class: 12b') == {'age': 12,
      'class': '12b',
      'name': 'Alex Fox'}
-    assert yaml('name: "Alex Fox"\n'
+    assert yaml_2('name: "Alex Fox"\n'
      'age: 12\n'
      '\n'
      'class: 12b') == {'age': 12,
      'class': '12b',
      'name': 'Alex Fox'}
-    assert yaml('name: "Alex \\"Fox\\""\n'
+    assert yaml_2('name: "Alex \\"Fox\\""\n'
      'age: 12\n'
      '\n'
      'class: 12b') == {'age': 12,
      'class': '12b',
      'name': 'Alex "Fox"'}
-    assert yaml('name: "Bob Dylan"\n'
+    assert yaml_2('name: "Bob Dylan"\n'
      'children: 6\n'
      'alive: false') == {'alive': False,
      'children': 6,
      'name': 'Bob Dylan'}
-    assert yaml('name: "Bob Dylan"\n'
+    assert yaml_2('name: "Bob Dylan"\n'
      'children: 6\n'
      'coding:') == {'children': 6,
      'coding': None,
      'name': 'Bob Dylan'}
-    assert yaml('name: "Bob Dylan"\n'
+    assert yaml_2('name: "Bob Dylan"\n'
      'children: 6\n'
      'coding: null') == {'children': 6,
      'coding': None,
      'name': 'Bob Dylan'}
-    assert yaml('name: "Bob Dylan"\n'
+    assert yaml_2('name: "Bob Dylan"\n'
      'children: 6\n'
      'coding: "null" ') == {'children': 6,
      'coding': 'null',
