8ae9bf62ca2e3935f5959e3d6de18a4dc097db77
DorogAD <sa_do@tut.by>
2021-01-17 10:30:36 +0300

    ice_base task01 finish

--- ice_base/task01_the_most_numbers.py
+++ ice_base/task01_the_most_numbers.py
@@ -16,7 +16,7 @@ Output: The difference between maximum and minimum as a number (int, float).
 
 
 def checkio(*args):
-    return 0
+    return max(args) - min(args) if args else 0
 
 
 # These "asserts" using only for self-checking and not necessary for auto-testing
