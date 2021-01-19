81b40b36a7bc4d09a992ae63297e3c3423050f55
DorogAD <sa_do@tut.by>
2021-01-19 13:40:47 +0300

    ice_base task05 finish

--- ice_base/task05_long_repeat.py
+++ ice_base/task05_long_repeat.py
@@ -13,11 +13,26 @@ def long_repeat(line: str) -> int:
     """
         length the longest substring that consists of the same char
     """
-    # your code here
-    return 0
+    if line == '':
+        return 0
+    previous = line[0]
+    count = 1
+    max_count = 1
+    for letter in line[1:]:
+        if letter == previous:
+            count += 1
+            if count > max_count:
+                max_count = count
+        elif letter != previous:
+            count = 1
+            previous = letter
+    return max_count
+
 
 if __name__ == '__main__':
-    #These "asserts" using only for self-checking and not necessary for auto-testing
+    # These "asserts" using only for self-checking and not necessary for auto-testing
+    assert long_repeat('a') == 1, "minus one"
+    assert long_repeat('aa') == 2, "Zero"
     assert long_repeat('sdsffffse') == 4, "First"
     assert long_repeat('ddvvrwwwrggg') == 3, "Second"
     assert long_repeat('abababaab') == 2, "Third"
