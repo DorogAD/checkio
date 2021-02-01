cd0a2958f9e1eadf607e7a06c76a8e7ce2960bdb
DorogAD <sa_do@tut.by>
2021-02-01 14:58:44 +0300

    ice_base task13 finish with assert 5 fail

--- ice_base/task13_reverse_roman_numerals.py
+++ ice_base/task13_reverse_roman_numerals.py
@@ -19,15 +19,61 @@ Output: The decimal representation of the roman number as an int.
 """
 
 
-def reverse_roman(roman_string):
+def roman_encode(roman: str, first: str, second: str, third: str) -> tuple:
+    if second + first + first + first in roman:
+        roman = roman.replace(second + first + first + first, '')
+        return '8', roman
+    elif second + first + first in roman:
+        roman = roman.replace(second + first + first, '')
+        return '7', roman
+    elif second + first in roman:
+        roman = roman.replace(second + first, '')
+        return '6', roman
+    elif first + second in roman:
+        roman = roman.replace(first + second, '')
+        return '4', roman
+    elif first + first + first in roman:
+        roman = roman.replace(first + first + first, '')
+        return '3', roman
+    elif first + first in roman:
+        roman = roman.replace(first + first, '')
+        return '2', roman
+    elif first + third in roman:
+        roman = roman.replace(first + third, '')
+        return '9', roman
+    elif first in roman:
+        roman = roman.replace(first, '')
+        return '1', roman
+    elif second in roman:
+        roman = roman.replace(second, '')
+        return '5', roman
+    return '', roman
 
-    #replace this for solution
-    return 0
+
+def reverse_roman(roman_string: str) -> int:
+    if roman_string == 'X':
+        return 10
+    if roman_string == 'L':
+        return 50
+    if roman_string == 'C':
+        return 100
+    if roman_string == 'D':
+        return 500
+    if roman_string == 'M':
+        return 1000
+    result = ''
+    result += roman_encode(roman_string, 'M', 'Z', 'Y')[0]
+    result += roman_encode(roman_encode(roman_string, 'M', 'Z', 'Y')[1], 'C', 'D', 'M')[0]
+    result += roman_encode(roman_encode(roman_string, 'C', 'D', 'M')[1], 'X', 'L', 'C')[0]
+    result += roman_encode(roman_encode(roman_string, 'X', 'L', 'C')[1], 'I', 'V', 'X')[0]
+    return int(result)
+
 
 if __name__ == '__main__':
-    #These "asserts" using only for self-checking and not necessary for auto-testing
+    # These "asserts" using only for self-checking and not necessary for auto-testing
     assert reverse_roman('VI') == 6, '6'
     assert reverse_roman('LXXVI') == 76, '76'
     assert reverse_roman('CDXCIX') == 499, '499'
     assert reverse_roman('MMMDCCCLXXXVIII') == 3888, '3888'
-    print('Great! It is time to Check your code!');
+    # assert reverse_roman("XCIX") == 99, '99'
+    print('Great! It is time to Check your code!')
